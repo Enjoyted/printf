681ca0bbab1722df77d9a880ca27069605f0b406
Enjoyted <enjoyted@gmail.com>
2016-05-04 18:11:14 +0200

    yes of course

--- main.c
+++ main.c
@@ -43,19 +43,14 @@ int my_printf(char *str, ...)
 
 int main()
 {
-  my_put_nbr(my_printf("1 %d %c %s %i %o\n", 'l', 'c', "dope", 4, 42));
-  my_putchar('\n');
-  my_put_nbr(printf("1 %d %c %s %i %o\n", 'l', 'c', "dope", 4, 42));
-  my_putchar('\n');  
-  
-  my_put_nbr(printf("1 %x %X %d%%\n", 5859, 9856, 85416));
-  my_putchar('\n');
-  my_put_nbr(my_printf("1 %x %X %d%%\n", 5859, 9856, 85416));
-  my_putchar('\n');
-  
-  my_putchar('\n');
-  my_put_nbr(my_printf("1 %o\n", 42));
-  my_putchar('\n');
-  my_put_nbr(my_printf("1 %u\n", 4200000000));
+  my_printf("1 - une chaine\n");
+  my_printf("2 - %s\n", "une autre chaine");
+  my_printf("3 - %i\n", 42);
+  my_printf("4 - %s %d %s%c", "avec", 4, "parametres", '\n');
+  my_printf("1 - %o\n", 42);         // unsigned octal
+  my_printf("2 - %u\n", 4200000000); // unsigned decimal
+  my_printf("3 - %x\n", 42);         // unsigned hexadecimal
+  my_printf("4 - %X\n", 42);         // unsigned hexadecimal 
+  my_printf("5 - %d%%\n", 42);
   return (0);
 }
